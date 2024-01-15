86a2004e79f873a16f88c728a9ba489a0dbef592
Victor Chaves <104216761+victorchavesm@users.noreply.github.com>
2024-01-15 12:25:58 -0300

    exercicio

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello, World! \u{1F30E}";
+        ?>
+    </h1>
+
+    <p>Tentando me livrar da maldição.</p>
+    
+</body>
+</html>
