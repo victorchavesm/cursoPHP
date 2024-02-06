c17cfc9efaeb2e0bac2bf7c8d332c14a1973b75e
Victor Chaves <104216761+victorchavesm@users.noreply.github.com>
2024-02-05 23:06:40 -0300

    exercicios

---
+++ ex009/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        //pegando os dados do formulário retroalimentado
+        $valor1 = $GET['v1'] ?? 0;
+        $valor2 = $GET['v2'] ?? 0;
+        
+    ?>
+    <main>
+        <h1>Somador de Valores</h1>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="get">
+            <label for="v1">Valor 1</label>
+            <input type="number" name="v1" id="v1" value="<?=$valor1// supertag abreviando echo?>">
+            <label for="v2">Valor 2</label>
+            <input type="number" name="v2" id="v2" value="<?=$valor2?>">
+            <input type="submit" value="Somar">
+        </form>
+    </main>
+    <section id="resultado">
+        <h2>Resultado da soma</h2>
+    </section>
+</body>
+</html>
